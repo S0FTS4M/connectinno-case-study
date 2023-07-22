c07fb7f868c7373a58355bb2e3094e81046a6053
Şamil <sml.ozclk@gmail.com>
2023-07-22 11:59:56 +0300

    Introduce GameManager

---
+++ Assets/Scripts/LevelManagement/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Zenject;
+
+public class GameManager : MonoBehaviour
+{
+    private IDataManager _dataManager;
+    private ILevelManager _levelManager;
+    private LevelButton.Factory _levelButtonFactory;
+
+    [Inject]
+    public void Construct(IDataManager dataManager, ILevelManager levelManager, LevelButton.Factory levelButtonFactory)
+    {
+        this._dataManager = dataManager;
+        this._levelManager = levelManager;
+        this._levelButtonFactory = levelButtonFactory;
+    }
+
+    private void Start()
+    {
+        
+    }
+}
