4798619e7de741292838094883d28bb20f1f1d1f
Şamil <sml.ozclk@gmail.com>
2023-07-22 11:59:43 +0300

    Introduce  player data

---
+++ Assets/Scripts/LevelManagement/PlayerData.cs
@@ -0,0 +1,12 @@
+[System.Serializable]
+public class PlayerData
+{
+    public int highestUnlockedLevel; // Represents the highest level unlocked by the player
+    public int[] highScores; // Array to store high scores for each level
+
+    public PlayerData(int totalLevels)
+    {
+        highestUnlockedLevel = 1; // Start with level 1 unlocked
+        highScores = new int[totalLevels];
+    }
+}
