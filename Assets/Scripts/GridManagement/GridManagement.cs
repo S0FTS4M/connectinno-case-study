d3ba359ccff4a17842b40249e90833fe67372396
Şamil <sml.ozclk@gmail.com>
2023-07-24 14:10:44 +0300

    prioritize target objectives to fill the grid

--- Assets/Scripts/GridManagement/GridManagement.cs
+++ Assets/Scripts/GridManagement/GridManagement.cs
@@ -41,45 +41,64 @@ public class GridManager : MonoBehaviour
 
         _grid = new Tile[level.row, level.column];
 
-        //NOTE: I need to make sure that the created items matches the objectives
-        var objectiveDict = new Dictionary<string, int>();
-        foreach (var objective in level.targetObjectives)
-        {
-            objectiveDict.Add(objective.name, objective.count);
-        }
 
         for (int i = 0; i < level.row; i++)
         {
             for (int j = 0; j < level.column; j++)
             {
+                if(_grid[i,j] != null)
+                    continue;
+
                 _grid[i, j] = _tilePool.Spawn();
                 _grid[i, j].transform.SetParent(_gridParent);
 
                 _grid[i, j].row = i;
                 _grid[i, j].col = j;
+            }
+        }
+
+        FillGridWithTargetObjectives(level.targetObjectives);
+        FillEmptyTilesWithRandomItems();
+    }
+
+    //NOTE: this can cause infinite loops so I need to check a lot of things here. But i will leave it here for now
+    private void FillGridWithTargetObjectives(List<TargetObjective> targetObjectives)
+    {
+        foreach (var objective in targetObjectives)
+        {
+            var itemData = _tileSettings.GetItemData(objective.name);
+
+            for (int i = 0; i < objective.count; i++)
+            {
+                var rndX = UnityEngine.Random.Range(0, _grid.GetLength(0));
+                var rndY = UnityEngine.Random.Range(0, _grid.GetLength(1));
+                while (_grid[rndX, rndY].ItemData != null)
+                {
+                    rndX = UnityEngine.Random.Range(0, _grid.GetLength(0));
+                    rndY = UnityEngine.Random.Range(0, _grid.GetLength(1));
+                }
+
+                _grid[rndX, rndY].SetItemData(itemData);
+            }
+        }
+    }
+
+    private void FillEmptyTilesWithRandomItems()
+    {
+        for (int i = 0; i < _grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < _grid.GetLength(1); j++)
+            {
+                if (_grid[i, j].ItemData != null)
+                    continue;
+
                 //select random item from itemDatas
                 var randomIndex = UnityEngine.Random.Range(0, _tileSettings.itemDatas.Length);
 
                 var randomItemData = _tileSettings.itemDatas[randomIndex];
                 _grid[i, j].SetItemData(randomItemData);
-
-                if (objectiveDict.ContainsKey(randomItemData.name))
-                    objectiveDict[randomItemData.name]--;
             }
         }
-
-        //NOTE: I will not implement this part yet, because this can cause infinite loops so I need to check a lot of things here.
-        // maybe I will come back to this later. Maybe not... So if you have a time please implement this.
-        // foreach (var objective in objectiveDict)
-        // {
-        //     if(objective.Value > 0)
-        //     {
-        //         var itemData = _tileSettings.GetItemData(objective.Key);
-        //     }
-
-
-        // }
-
     }
 
     private void DespawnAllTiles()
