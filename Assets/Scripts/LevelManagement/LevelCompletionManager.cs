86baab6e63cea8ea07acf2842ca3089d5ee27ba7
Şamil <sml.ozclk@gmail.com>
2023-07-25 14:16:51 +0300

    implement lose conditions to Completion manager

--- Assets/Scripts/LevelManagement/LevelCompletionManager.cs
+++ Assets/Scripts/LevelManagement/LevelCompletionManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using Zenject;
 
@@ -9,6 +10,12 @@ public class LevelCompletionManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject highScoreUI;
+
+    [SerializeField]
+    private GameObject failInfoUI;
+
+    [SerializeField]
+    private TextMeshProUGUI failInfo;
 
     private ILevelManager _levelManager;
     private LevelUI _levelUI;
@@ -25,7 +32,10 @@ public class LevelCompletionManager : MonoBehaviour
         _levelSelectionUI = levelSelectionUI;
         _dataManager = dataManager;
 
+        _levelManager.NoAvailableSetFound += OnNoAvailableSetFound;
+        levelManager.PlayerOutOfMoves += OnPlayerOutOfMoves;
         levelManager.LevelCompleted += OnLevelCompleted;
+        levelManager.LevelFailed += OnLevelFailed;
     }
 
     private void OnLevelCompleted(LevelData levelData, bool newHighScore)
@@ -33,17 +43,17 @@ public class LevelCompletionManager : MonoBehaviour
         var seq = DOTween.Sequence();
         seq.AppendCallback(_levelUI.HideUI);
         seq.AppendInterval(.5f);
-        seq.AppendCallback(() => highScoreUI.SetActive(true));
-        seq.AppendCallback(() => levelCompleteParticleSystem.Play());
-        seq.Append(highScoreUI.transform.DOScale(1.1f, .5f).SetLoops(4, LoopType.Yoyo));
         Action levelSelectionUIAction = null;
         if (newHighScore)
         {
+            seq.AppendCallback(() => highScoreUI.SetActive(true));
+            seq.AppendCallback(() => levelCompleteParticleSystem.Play());
+            seq.Append(highScoreUI.transform.DOScale(1.1f, .5f).SetLoops(4, LoopType.Yoyo));
+            seq.AppendCallback(() => highScoreUI.SetActive(false));
             levelSelectionUIAction = () => _levelSelectionUI.SetLevelPlayable(levelData.levelNumber + 1);
             _dataManager.SetHighestUnlockedLevel(levelData.levelNumber + 1);
         }
 
-        seq.AppendCallback(() => highScoreUI.SetActive(false));
         seq.AppendCallback(() =>
         _levelSelectionUI.ShowUI(
             () => levelSelectionUIAction?.Invoke()
@@ -51,4 +61,25 @@ public class LevelCompletionManager : MonoBehaviour
 
         _levelSelectionUI.UpdateHighScore(levelData.levelNumber);
     }
+
+    private void OnPlayerOutOfMoves()
+    {
+        failInfo.SetText("Out Of Moves");
+        failInfoUI.SetActive(true);
+        failInfoUI.transform.DOScale(1.1f, .5f).SetLoops(4, LoopType.Yoyo).OnComplete(() => failInfoUI.SetActive(false));
+    }
+
+    private void OnNoAvailableSetFound()
+    {
+        failInfo.SetText("No Available Set");
+        failInfoUI.SetActive(true);
+        failInfoUI.transform.DOScale(1.1f, .5f).SetLoops(4, LoopType.Yoyo).OnComplete(() => failInfoUI.SetActive(false));
+    }
+
+    private void OnLevelFailed(LevelData data)
+    {
+        _levelUI.HideUI();
+        _levelSelectionUI.ShowUI();
+    }
+
 }
